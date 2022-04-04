f75b7c775edcfdeb0ceeec207de6cae3143c596a
NungWinPP <parinthon10031@gmail.com>
2022-04-04 05:23:53 +0300

    Show onboarding only once

---
+++ Bankey/Utils/LocalState.swift
@@ -0,0 +1,25 @@
+//
+//  LocalState.swift
+//  Bankey
+//
+//  Created by Parinthon Puksuriwong on 4/4/2565 BE.
+//
+
+import Foundation
+
+public class LocalState {
+    
+    private enum Keys: String {
+        case hasOnboarded
+    }
+    
+    public static var hasOnboarded: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: Keys.hasOnboarded.rawValue)
+        }
+        set(newValue) {
+            UserDefaults.standard.set(newValue, forKey: Keys.hasOnboarded.rawValue)
+            UserDefaults.standard.synchronize()
+        }
+    }
+}
